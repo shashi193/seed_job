a535503f8095c5129a91c59683de46a32b8986a5
shashi193 <shashi.ujjwal121@gmail.com>
2017-03-22 23:18:50 +0530

    Create seed_job.groovy

---
+++ seed_job.groovy
@@ -0,0 +1,12 @@
+multibranchPipelineJob('Multi_Job_Test') {
+    branchSources {
+        git {
+            remote('https://github.com/shashi193/multibranch_workflow.git')
+          
+        }
+    }
+    orphanedItemStrategy {
+        discardOldItems {
+            numToKeep(20)
+        }
+    }
